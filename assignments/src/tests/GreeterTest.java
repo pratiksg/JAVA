40cb691d5763126ae4a68724f3548482545e3d26
GAIKWAD <pratiksha-bhaskar.gaikwad@capgemini.com>
2019-03-11 19:11:04 +0530

    first commit

---
+++ assignments/src/tests/GreeterTest.java
@@ -0,0 +1,17 @@
+package tests;
+import org.junit.Test;
+import main.Greeter;
+import static org.junit.Assert.*;
+public class GreeterTest{
+	@Test
+	public void TestGreet(){
+	
+	assertEquals("Hello",new Greeter().greet());
+
+		
+	}
+
+
+
+
+}
---
+++ assignments/src/tests/GreetorTest.java
@@ -0,0 +1,17 @@
+package tests;
+import org.junit.tests;
+import main.Greetor;
+import static org.junit Assert.*;
+public GreetorTest{
+	@Tests
+	public void TestGreek(){
+	
+	assertEquals("Hello",new Greetor().greet());
+
+
+	}
+
+
+
+
+}
